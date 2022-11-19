6bc8f07078db3f4234a36106bdea5cd33b23a0a0
agavirja <gaviriaalejandro@hotmail.com>
2022-11-18 22:38:30 -0500

    actualizar ficha inmueble

--- ficha_inmueble.py
+++ ficha_inmueble.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 # streamlit run D:\Dropbox\Empresa\Buydepa\COLOMBIA\DESARROLLO\streamlit\portal_inmobiliario\ficha_inmueble.py
 
+# https://agavirja-buydepa-property-ficha-inmueble-66hgl1.streamlit.app/
+# https://agavirja-buydepa-property-ficha-inmueble-66hgl1.streamlit.app/?option=15435
+
 # query params exist
 try:
-    options = ['cat', 'dog', 'mouse', 'bat', 'duck']
-
     query_params = st.experimental_get_query_params()
     query_option = query_params['option'][0] #throws an exception when visiting http://host:port
-
     st.text(query_option)
     st.text('id_inmueble')
 
